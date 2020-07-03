316508c1900d7da0d5a6f98008636e2c863379d3
Simone Bisogno <s.bisogno10@studenti.unisa.it>
2020-07-03 16:20:29 +0200

    Update to version 0.0.1-dev+20200703+1602 Update JGraphT to 1.5.0

--- python/setup.py
+++ python/setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fly-graph",
-    version="0.0.1-dev+20200703",
+    version="0.0.1-dev+20200703+1602",
     author="Simone Bisogno",
     author_email="s.bisogno90@gmail.com",
     description="A graph library for FLY language, written in Python",
