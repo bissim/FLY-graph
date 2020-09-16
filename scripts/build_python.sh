59bec67979dd206f792ef756d7bfb4cb94c4bdc4
Simone Bisogno <s.bisogno10@studenti.unisa.it>
2020-09-16 23:36:49 +0200

    Fix build python script for Travis CI

--- scripts/build_python.sh
+++ scripts/build_python.sh
@@ -4,14 +4,14 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+pwd
 cd ./python/
+pwd
 python3 -m pip install --upgrade setuptools wheel
 python3 setup.py sdist bdist_wheel
 ls -la dist/
 python3 -m pip install -r fly_graph.egg-info/requires.txt
-cd ..
-echo
-python3 -m python.fly.graph.tests
+python3 -m fly.graph.tests
 python3 -m pip install coverage
 python3 -m coverage report -i fly/graph/*
 python3 -m coverage xml -i fly/graph/*
