4e41c1a32156c12e603fe5f1f973cf20ba5c5a7b
Simone Bisogno <s.bisogno10@studenti.unisa.it>
2020-09-17 00:19:10 +0200

    Fix coverage installation on Travis CI for Python project

--- scripts/build_python.sh
+++ scripts/build_python.sh
@@ -15,10 +15,10 @@ ls -la dist/
 
 echo -e "\nRunning FLY Graph tests"
 python3 -m pip install -r fly_graph.egg-info/requires.txt
+python3 -m pip install coverage
 python3 -m coverage run fly.graph.tests
 
 echo -e "\nReport FLY Graph code coverage"
-python3 -m pip install coverage
 python3 -m coverage report -i fly/graph/*
 python3 -m coverage xml -i fly/graph/*
 ls -la
