703491eb363cfc2915b133bf5fc18d1774d250b4
Simone B <s.bisogno10@studenti.unisa.it>
2021-03-13 18:51:39 +0100

    Update Python project setup
    - Introduce ‛pyproject.toml‛ file as per current PyPA guidelines for packaging Python projects
    - Update ‛setup.py‛:
      - Bump minimum Python version to 3.7
      - Attempt to workaround Windows inability to handle symbolic links to properly package project licence (it will rarely be needed anyway)
      - Apply the ‛src‛ layout by properly declaring ‛package_dir‛ and ‛packages‛ options for ‛setup()‛ function
      - Programmatically generate project URLs
    
    [skip travis]

--- python/setup.py
+++ python/setup.py
@@ -11,10 +11,10 @@ setup.py for human
 
 import io
 import os
-import sys
-from shutil import rmtree
+import platform
+from shutil import copy2
 
-from setuptools import find_namespace_packages, setup
+from setuptools import find_packages, setup
 
 # Package meta-data
 NAME = 'fly-graph'
@@ -22,7 +22,7 @@ DESCRIPTION = 'A graph library for FLY language, written in Python'
 URL = 'https://github.com/bissim/FLY-graph'
 EMAIL = 's.bisogno90@gmail.com'
 AUTHOR = 'Simone Bisogno'
-REQUIRES_PYTHON = '~=3.6'
+REQUIRES_PYTHON = '~=3.7'
 
 # Required packages
 REQUIRED = [
@@ -32,12 +32,25 @@ REQUIRED = [
 # Optional packages
 EXTRAS = {}
 
+# Source directory
+SOURCE_DIR = "src"
+
+HERE = os.getenv('HERE', default=os.path.abspath(os.path.dirname(__file__)))
+
+# Handle with licence symlink on Windows
+SEP = os.path.sep
+if platform.system() == 'Windows':
+    try:
+        os.rename(f"{HERE}{SEP}LICENCE", f"{HERE}{SEP}LICENCE.bak")
+        copy2(f"..{SEP}LICENCE", f".{SEP}LICENCE")
+    except FileNotFoundError as fnfe:
+        print(fnfe)
+
 # The rest you shouldn't have to touch too much :)
 # ------------------------------------------------
 # Except, perhaps the License and Trove Classifiers!
-# If you do change the License, remember to change the Trove Classifier for that!
-
-HERE = os.path.abspath(os.path.dirname(__file__))
+# If you do change the License, remember to change
+# the Trove Classifier for that!
 
 # Import the README and use it as the long-description
 # Note: this will only work if 'README.md' is present in your MANIFEST.in file!
@@ -45,16 +58,19 @@ try:
     with io.open(os.path.join(HERE, 'README.md'), encoding='utf-8') as f:
         LONG_DESCRIPTION = '\n' + f.read()
 except FileNotFoundError:
+    print("No long description found.")
     LONG_DESCRIPTION = DESCRIPTION
-f.close()
 del f
 
 PARENT = os.path.dirname(HERE)
 
 # Import the VERSION and use it as the version
-with io.open(os.path.join(PARENT, 'VERSION'), encoding='utf-8') as f:
-    VERSION = f.readline().rstrip('\n')
-f.close()
+try:
+    with io.open(os.path.join(PARENT, 'VERSION'), encoding='utf-8') as f:
+        VERSION = f.readline().rstrip('\n')
+except FileNotFoundError as fnfe:
+    print(f"{fnfe}")
+    raise
 del f
 
 # Where the magic happens:
@@ -68,9 +84,8 @@ setup(
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
-    packages=find_namespace_packages(
-        exclude=["tests", "*.tests", "*.tests.*", "tests.*"]
-    ),
+    package_dir={"": SOURCE_DIR},
+    packages=find_packages(where=SOURCE_DIR),
     install_requires=REQUIRED,
     extras_require=EXTRAS,
     include_package_data=True,
@@ -89,7 +104,15 @@ setup(
     ],
     keywords='FLY graph management',
     project_urls={
-        'Source': 'https://github.com/bissim/FLY-graph',
-        'Tracker': 'https://github.com/bissim/FLY-graph/issues'
+        'Source': URL,
+        'Tracker': f"{URL}/issues"
     },
 )
+
+# Restore licence symlink on Windows
+if platform.system() == 'Windows':
+    try:
+        os.remove(f".{SEP}LICENCE")
+        os.rename(f".{SEP}LICENCE.bak", f".{SEP}LICENCE")
+    except FileNotFoundError as fnfe:
+        print(fnfe)
