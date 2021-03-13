6db4a29b29033e00ba99f2d34511bb0715be45d2
Simone B <s.bisogno10@studenti.unisa.it>
2021-03-13 18:55:35 +0100

    Introduce Tox to run tests in Python project
    - Add ‛.tox‛ folder as ignored path
    - Introduce ‛tox.ini‛ file to configure Tox execution in order to run both unit tests ans code coverage reports
    - Drop ‛unittest‛ in favour of ‛pytest‛ package to perform unit testing
    - Update ‛.coveragerc‛ according to Pytest usage, along with ‛pytest-cov‛ package
    - Update both ‛build_python.sh‛ script used by TravisCI and ‛python-publish.yml‛ workflow for GitHub Actions in order to reflect the Tox introduction to Python project

--- scripts/build_python.sh
+++ scripts/build_python.sh
@@ -9,17 +9,10 @@ cd ./python/
 pwd
 
 echo -e "\nBuilding FLY Graph project"
-python3 -m pip install --upgrade setuptools wheel
-python3 setup.py sdist bdist_wheel
+python3 -m pip install --upgrade pip setuptools build wheel tox
+python3 -m build
 ls -la dist/
 
-echo -e "\nRunning FLY Graph tests"
-python3 -m pip install -r fly_graph.egg-info/requires.txt
-python3 -m fly.graph.tests
-
-echo -e "\nReport FLY Graph code coverage"
-python3 -m pip install coverage
-python3 -m coverage run fly/graph/graph.py --omit=examples,tests
-python3 -m coverage report -i fly/graph/graph.py --omit=examples,tests
-python3 -m coverage xml -i fly/graph/graph.py --omit=examples,tests
+echo -e "\nRunning FLY Graph tests and code coverage"
+python3 -m tox -e cover
 ls -la
