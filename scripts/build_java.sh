e6345457a94151a04efaa22a3b83257037c2af0f
Simone B <s.bisogno10@studenti.unisa.it>
2021-03-13 19:16:11 +0100

    Update TravisCI scripts
    - In ‛build_java.sh‛:
      - Add console message
    
    - In ‛build_python.sh‛:
      - Remove ‛pwd‛ instructions inserted for test purposes
      - Remove Pythonproject building as it is no more required
      - Remove final ‛ls -la‛ instruction inserted for test purposes

--- scripts/build_java.sh
+++ scripts/build_java.sh
@@ -5,4 +5,6 @@ set -euo pipefail
 IFS=$'\n\t'
 
 cd ./java/
+
+echo -e "\nRunning FLY Graph tests and code coverage"
 mvn -T $(nproc) clean package
--- scripts/build_python.sh
+++ scripts/build_python.sh
@@ -4,15 +4,7 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-pwd
 cd ./python/
-pwd
-
-echo -e "\nBuilding FLY Graph project"
-python3 -m pip install --upgrade pip setuptools build wheel tox
-python3 -m build
-ls -la dist/
 
 echo -e "\nRunning FLY Graph tests and code coverage"
 python3 -m tox -e cover
-ls -la
